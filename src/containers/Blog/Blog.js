b30b54727b8c6dadd44dee460bd0ca47583727ba
Qurat Ul Ain Mumtaz <mcs.qurat@gmail.com>
2020-08-26 16:54:11 +0500

    my seconed commit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,78 +1,35 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
+import Posts from './Posts/Posts';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+
 import './Blog.css';
 
 
 
 class Blog extends Component {
-   state={
-       posts: [],
-       selectedPostId:null,
-       error:false
-   }
+  
 
 
-    componentDidMount(){
-        
-      axios.get('/posts')
-      .then(response=>{
-          const posts = response.data.slice(0, 4);
-          const updatedPosts= posts.map(post=>{
-            return{
-                ...post,
-                author:'Max'
-            }
-          });
-          this.setState({posts:updatedPosts});
-        //   console.log(Response);
-
-      }).catch(error=>{
-        //   
-        this.setState({error:true})
-      });
-    }
-    postSelectedHandler=(id)=>{
-        this.setState({selectedPostId:id})
-
-
-    }
+    
 
 
 
     render () {
-        let posts = <p style={{textAlign:"center"}}>Some thing went wrong.....</p>
-        if(!this.state.error){
-            posts = this.state.posts.map(post=>{
-                return  <Post key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={()=>this.postSelectedHandler(post.id)}/>
-              });
-              // const posts = this.state.posts.map(post=>{
-              //     return  <Post  />;
       
-      
-              // });
-
-        }
      
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                   
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="block">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/NewPost">New Post</a></li>
+                            
+                        </ul>
+                    </nav>
+                </header>
+                <Posts />
+                
             </div>
         );
     }
